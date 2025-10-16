76d420c9d27d6e198808f904def9faceb2850558
Igor Ferreira <igorkmail@gmail.com>
2025-10-16 01:23:28 -0300

    Apply theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nordras.github.io',
-	base: '/main',
-	integrations: [mdx(), sitemap()],
+  site: 'https://nordras.github.io',
+  base: '/main',
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'DevBlog - Desenvolvimento Web';
+export const SITE_DESCRIPTION = 'Artigos, tutoriais e dossiers sobre JavaScript, React, Node.js e tecnologias modernas de desenvolvimento web.';
